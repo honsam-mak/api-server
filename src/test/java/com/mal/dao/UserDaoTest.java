24862e04d9e68d161b5b02b00ab0f573c3519ab6
Honsam Mak <honsam@gmail.com>
2014-08-05 17:42:46 +0800

    add spring injection framework config into pom.xml and the sample code

--- src/test/java/com/mal/dao/UserDaoTest.java
+++ src/test/java/com/mal/dao/UserDaoTest.java
@@ -1,11 +1,29 @@
 package com.mal.dao;
 
+import javax.inject.Inject;
+
+import org.junit.Assert;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
-public class UserDaoTest {
-	public UserDaoTest() {}
+import com.mal.config.DatabaseConfig;
+import com.mal.orm.User;
+
+@ContextConfiguration(
+    classes={DatabaseConfig.class},
+    initializers=com.mal.config.AppContextIntializer.class
+)
+public class UserDaoTest extends AbstractTestNGSpringContextTests {
+
+	@Inject
+	private UserDao userDao;
 
 	@Test
 	public void addUser() {
+
+		User user = userDao.find(2L);
+
+		Assert.assertNotNull(user);
 	}
 }
